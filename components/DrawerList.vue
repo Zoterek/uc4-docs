013ba61c04723781e0fb06133349b06c385ae796
Zoterek <pistawecki@gmail.com>
2018-08-23 14:41:00 +0200

    initial commit

---
+++ components/DrawerList.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-list dense>
+    <template
+      v-for="item in items"
+      :item="item">
+    <v-list-group
+      v-if="item.children"
+      :key="item.text">
+      <v-list-tile slot="activator">
+        <v-list-tile-content>
+          <v-list-tile-title>
+            {{ item.text }}
+          </v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile
+        v-for="child in item.children"
+        :to="child.to"
+        :key="child.text"
+        @click="click()">
+        <v-list-tile-content>
+          <v-list-tile-title>
+            {{ child.text }}
+          </v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list-group>
+    <v-list-tile
+      v-else
+      :to="item.to"
+      :key="item.text"
+      @click="click()">
+      <v-list-tile-content>
+        <v-list-tile-title>
+          {{ item.text }}
+        </v-list-tile-title>
+      </v-list-tile-content>
+    </v-list-tile>
+    </template>
+  </v-list>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      click: function() {
+
+      }
+    }
+  },
+  props: {
+    items: Array
+  }
+}
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      :clipped="$vuetify.breakpoint.lgAndUp"
+      app
+      fixed>
+      <drawer-list
+        :items="items">
+      </drawer-list>
+    </v-navigation-drawer>
+    <v-toolbar
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      app
+      dark
+      fixed>
+      <v-toolbar-title>
+        <span>Docs</span>
+      </v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+        <nuxt></nuxt>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import DrawerList from '@/components/DrawerList'
+
+export default {
+  data () {
+    return {
+      items: [
+        { text: "Getting Started", to: "/" },
+        { text: "Configuration",
+          children: [
+            { text: "config.json", to: "/configuration/config" },
+            { text: "filter.json", to: "/configuration/filter" }
+          ] },
+        { text: "Tools",
+          children: [
+            { text: "uc4_search" },
+            { text: "uc4_regex_replace" }
+          ]
+        }
+      ],
+      c: function() {
+
+      }
+    }
+  },
+  name: 'App',
+  components: {
+    DrawerList
+  }
+}
+</script>
