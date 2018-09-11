92dc367eb3ede12db54b0f4ef0948c27fc99b56d
Zoterek <pistawecki@gmail.com>
2018-09-11 16:48:35 +0200

    add store/other functionality

--- pages/index.vue
+++ pages/index.vue
@@ -1,51 +1,45 @@
 <template>
-  <v-card class="elevation-8">
-    <v-toolbar dark>
-      <v-toolbar-title>{{ title }}</v-toolbar-title>
-    </v-toolbar>
-    <v-card-text>
-      <v-container fluid>
-        <v-layout align-center row>
-          <v-flex xs6>
-            <v-select
-              v-model="select"
-              :items="appNames"
-              label="Select App"
-            ></v-select>
+  <v-card-text>
+    <v-container fluid>
+      <v-layout align-center row>
+        <v-flex xs6>
+          <v-select
+            v-model="app"
+            :items="appNames"
+            label="Select App"
+            dense
+            required
+            :error="error"
+            :rules="appRules"></v-select>
+        </v-flex>
+        <transition name="fade">
+          <v-flex xs6 class="text-xs-right"
+            v-if="app">
+            <v-tooltip
+              top
+              v-for="external in selectedApp.external"
+              :key="external.name">
+              <v-btn class="grey--text"
+                icon
+                slot="activator">
+                <v-icon :class="external.icon"
+                  ></v-icon>
+              </v-btn>
+              <span>{{ external.tooltip }}</span>
+            </v-tooltip>
           </v-flex>
-          <transition name="fade">
-            <v-flex xs6 class="text-xs-right"
-              v-if="select">
-              <v-tooltip
-                top
-                v-for="external in selectedApp.external"
-                :key="external.name">
-                <v-btn class="grey--text"
-                  icon
-                  slot="activator">
-                  <v-icon :class="external.icon"
-                    ></v-icon>
-                </v-btn>
-                <span>{{ external.tooltip }}</span>
-              </v-tooltip>
-            </v-flex>
-          </transition>
-        </v-layout>
-        <v-layout class="description" row>
-          <v-slide-y-transition>
-            <v-flex
-              v-if="select">
-              <component :is="selectedApp.description"></component>
-            </v-flex>
-          </v-slide-y-transition>
-        </v-layout>
-      </v-container>
-    </v-card-text>
-    <v-card-actions>
-      <v-spacer></v-spacer>
-      <v-btn color="primary" @click="select = ''">Next</v-btn>
-    </v-card-actions>
-  </v-card>
+        </transition>
+      </v-layout>
+      <v-layout class="description" row>
+        <v-slide-y-transition>
+          <v-flex
+            v-if="app">
+            <component :is="selectedApp.description"></component>
+          </v-flex>
+        </v-slide-y-transition>
+      </v-layout>
+    </v-container>
+  </v-card-text>
 </template>
 
 <style>
@@ -68,19 +62,50 @@ export default {
   data() {
     return {
       title: 'Automic App Docs',
-      select: '',
-      apps: []
+      appRules: [
+        e => !!e || 'App is required'
+      ],
+      apps: appList,
+      error: false
     }
   },
-  created: function() {
-    this.apps = appList
+  created() {
+    this.$store.commit('updateTitle', this.title)
+    this.$router.app.$on('nextStep', this.validate)
+    
+    this.$store.commit('updateStep', 0)
+  },
+  destroyed() {
+    this.$router.app.$off('nextStep')
   },
   computed: {
+    app: {
+      get: function() {
+        return this.$store.state.app
+      },
+      set: function(value) {
+        this.$store.commit('updateApp', value)
+      }
+    },
     appNames: function() {
       return this.apps.map(e => e.name)
     },
     selectedApp: function() {
-      return this.select ? this.apps.filter(e => e.name == this.select)[0] : {}
+      if(this.app) {
+        this.error = false
+        return this.apps.filter(e => e.name == this.app)[0]
+      }
+
+      return null
+    }
+  },
+  methods: {
+    validate: function() {
+      if(this.selectedApp) {
+        this.$router.push(this.selectedApp.to)
+      } else {
+        this.error = true
+      }
     }
   }
 }
--- store/index.js
+++ store/index.js
@@ -1,9 +1,31 @@
 export const state = () => ({
-  sidebar: false
+  title: '',
+  app: null,
+  step: 0,
+  userInput: {
+    client: 0,
+    users: '',
+    groups: ''
+  }
 })
 
 export const mutations = {
-  toggleSidebar (state) {
-    state.sidebar = !state.sidebar
+  updateTitle(state, title) {
+    state.title = title.toLowerCase().replace(/(\w{3,})/g, e => e[0].toUpperCase() + e.substring(1))
+  },
+  updateApp(state, value) {
+    state.app = value
+  },
+  updateClient(state, value) {
+    state.userInput.client = value
+  },
+  updateUsers(state, value) {
+    state.userInput.users = value
+  },
+  updateGroups(state, value) {
+    state.userInput.groups = value
+  },
+  updateStep(state, value) {
+    state.step = value
   }
 }
