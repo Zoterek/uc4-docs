7927873f2adc08c86792e6bcb38548209deafefe
Zoterek <17502624+Zoterek@users.noreply.github.com>
2018-09-13 15:21:27 +0200

    update url

--- assets/app-list.js
+++ assets/app-list.js
@@ -7,8 +7,8 @@ export default [
     description: AddUserToGroup,
     to: '/addusertogroup',
     external: [
-      { name: 'git', icon: 'fab fa-github', url: '', tooltip: 'github' },
-      { name: 'jar', icon: 'fas fa-file-download', url: '', tooltip: '.jar' }
+      { name: 'git', icon: 'fab fa-github', url: 'https://github.com/Zoterek/uc4_addUserToGroup', tooltip: 'github' },
+      { name: 'jar', icon: 'fas fa-file-download', url: 'https://github.com/Zoterek/uc4_addUserToGroup/releases/download/v1.0.0/uc4_addUserToGroup-1.0.0.zip', tooltip: '.zip' }
     ]
   }
 ]
